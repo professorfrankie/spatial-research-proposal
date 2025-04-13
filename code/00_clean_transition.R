46066b77a62712d3782689729a1488ed00254a34
professorfrankie <h12329154@s.wu.ac.at>
2025-04-13 11:14:57 +0200

    .

--- code/00_clean_transition.R
+++ code/00_clean_transition.R
@@ -4,16 +4,4 @@ library(stringr)
 library(sf)
 
 
-transition <- read_csv("raw_data/transitions_munis.csv") |> 
-  transmute(muni_id = CD_MUN, 
-            year = substr(bandName, 12, 20), 
-            year = str_replace_all(bandName, "_", ""), 
-            histogram = replace(histogram, histogram == "{}", NA), 
-            histogram = str_replace_all(histogram, "[{}]", "")) |> 
-  filter(!is.na(histogram)) |> 
-  separate_rows(histogram, sep = ",") |> 
-  separate(histogram, into = c("transition_id", "area"), sep = "=") |>  
-  mutate(transition_id = as.numeric(transition_id)) |> 
-  transmute(muni_id, year, transition_id, 
-            area_ha = as.numeric(area) * 30^2 / 10^4) |> 
-  arrange(muni_id, year)
+transition <- readRDS("raw_data/land_use_change_v9.rds")
