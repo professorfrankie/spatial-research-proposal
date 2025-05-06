bfdc6c630ab8fe11f89fd3cf36df63ad6cb164b4
professorfrankie <h12329154@s.wu.ac.at>
2025-05-06 23:02:42 +0200

    updated fixed effect feols

--- code/03_bartik.R
+++ code/03_bartik.R
@@ -110,75 +110,6 @@ control_var <- setdiff(names(df_model), c("muni_id", "year", "artisanal_mining_a
                                           "forest_loss_all_gross", "forest_loss_all_net"))
 
 
-##### REDUCED FORM #####
-
-# lag t-1
-# Build formula
-fml1 <- reformulate(c("bartik", control_var), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs1 <- paste(all.vars(fml1)[-1], collapse = " + ")
-full_formula1 <- as.formula(paste("forest_loss_all_gross ~", rhs1, "| year"))
-# Estimate fixed effects model
-modelt1 <- feols(full_formula1, data = df_model)
-summary(modelt1)
-
-# lag t-2
-# Build formula
-fml2 <- reformulate(c("bartik2", control_var), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects                      
-rhs2 <- paste(all.vars(fml2)[-1], collapse = " + ")
-full_formula2 <- as.formula(paste("forest_loss_all_gross ~", rhs2, "| year"))
-# Estimate fixed effects model
-modelt2 <- feols(full_formula2, data = df_model)
-summary(modelt2)
-
-# lag t-3
-# Build formula
-fml3 <- reformulate(c("bartik3", control_var), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs3 <- paste(all.vars(fml3)[-1], collapse = " + ")
-full_formula3 <- as.formula(paste("forest_loss_all_gross ~", rhs3, "| year"))
-# Estimate fixed effects model
-modelt3 <- feols(full_formula3, data = df_model)
-summary(modelt3)
-
-# lag t-4
-# Build formula
-fml4 <- reformulate(c("bartik4", control_var), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs4 <- paste(all.vars(fml4)[-1], collapse = " + ")
-full_formula4 <- as.formula(paste("forest_loss_all_gross ~", rhs4, "| year"))
-# Estimate fixed effects model
-modelt4 <- feols(full_formula4, data = df_model)
-summary(modelt4)
-
-
-# Example with feols models
-modelsRF <- list(
-  "t-1" = modelt1,
-  "t-2" = modelt2,
-  "t-3" = modelt3,
-  "t-4" = modelt4
-)
-
-modelsummary(
-  modelsRF,
-  output = "latex",
-  title = "Reduced Form Estimates",
-  coef_map = c(
-    "bartik" = "Bartik",
-    "bartik2" = "Bartik",
-    "bartik3" = "Bartik",
-    "bartik4" = "Bartik"
-  ),
-  statistic = c("({std.error})", "p.value"),  # similar to stargazer's default
-  stars = TRUE,
-  gof_omit = "AIC|BIC|Log.Lik|Deviance|RMSE",  # omit extra stats
-  add_rows = NULL,
-  escape = FALSE  # to allow LaTeX symbols in labels
-)
-
-
 ########## FIRST STAGE ##########
 
 ### dependent variable change in area ###
@@ -238,51 +169,29 @@ modelsummary(
 
 ###### SECOND STAGE #########
 
-# Build second-stage (2SLS) formula
-
-rhs_controls <- paste(control_var, collapse = " + ")
-
-
-dep~ind | fe | gghg
-
-
 # second stage for change_in_area
-dm1_hat <- fitted.values(first_stage1b)
-df_model$dm1_hat <- dm1_hat
-fml1B <- reformulate(c("dm1_hat", rhs_controls), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs1B <- paste(all.vars(fml1B)[-1], collapse = " + ")
-full_formula1B <- as.formula(paste("forest_loss_all_gross ~", rhs1B, "| year"))
-# Estimate fixed effects model
-second_stage1B <- feols(full_formula1B, data = df_model)
+second_stage1B <- feols(forest_loss_all_gross ~ spei_dry + gdp_pc_change + 
+                        population_change + pop_dens_change + pa_tot_ha_change + 
+                        n_fined_change + brl_fined_change | year | change_in_area ~ bartik,
+                        data = df_model)
 summary(second_stage1B)
 
-dm2_hat <- fitted.values(first_stage2b)
-df_model$dm2_hat <- dm2_hat
-fml2B <- reformulate(c("dm2_hat", rhs_controls), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs2B <- paste(all.vars(fml2B)[-1], collapse = " + ")
-full_formula2B <- as.formula(paste("forest_loss_all_gross ~", rhs2B, "| year"))
-# Estimate fixed effects model
-second_stage2B <- feols(full_formula2B, data = df_model)
+second_stage2B <- feols(forest_loss_all_gross ~ spei_dry + gdp_pc_change + 
+                        population_change + pop_dens_change + pa_tot_ha_change + 
+                        n_fined_change + brl_fined_change | year | change_in_area ~ bartik2,
+                        data = df_model)
 summary(second_stage2B)
-dm3_hat <- fitted.values(first_stage3b)
-df_model$dm3_hat <- dm3_hat
-fml3B <- reformulate(c("dm3_hat", rhs_controls), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs3B <- paste(all.vars(fml3B)[-1], collapse = " + ")
-full_formula3B <- as.formula(paste("forest_loss_all_gross ~", rhs3B, "| year"))
-# Estimate fixed effects model
-second_stage3B <- feols(full_formula3B, data = df_model)
+
+second_stage3B <- feols(forest_loss_all_gross ~ spei_dry + gdp_pc_change + 
+                        population_change + pop_dens_change + pa_tot_ha_change + 
+                        n_fined_change + brl_fined_change | year | change_in_area ~ bartik3,
+                        data = df_model)
 summary(second_stage3B)
-dm4_hat <- fitted.values(first_stage4b)
-df_model$dm4_hat <- dm4_hat
-fml4B <- reformulate(c("dm4_hat", rhs_controls), response = "forest_loss_all_gross")
-# Convert formula to character, then append fixed effects
-rhs4B <- paste(all.vars(fml4B)[-1], collapse = " + ")
-full_formula4B <- as.formula(paste("forest_loss_all_gross ~", rhs4B, "| year"))
-# Estimate fixed effects model
-second_stage4B <- feols(full_formula4B, data = df_model)
+
+second_stage4B <- feols(forest_loss_all_gross ~ spei_dry + gdp_pc_change + 
+                        population_change + pop_dens_change + pa_tot_ha_change + 
+                        n_fined_change + brl_fined_change | year | change_in_area ~ bartik4,
+                        data = df_model)
 summary(second_stage4B)
 
 # Combine second-stage models into a list
@@ -296,16 +205,13 @@ second_stage_models_change <- list(
 modelsummary(
   second_stage_models_change,
   output = "latex",
-  title = "Second Stage Estimates - Change in Area",
+  title = "Second Stage Estimates – Change in Area",
   coef_map = c(
-    "dm1_hat" = " minig",
-    "dm2_hat" = " minig",
-    "dm3_hat" = " minig",
-    "dm4_hat" = " minig"
+    "fit_change_in_area" = "Change in Garimpo Area"
   ),
-  statistic = c("({std.error})", "p.value"),  # similar to stargazer's default
+  statistic = c("({std.error})", "p.value"),
   stars = TRUE,
-  gof_omit = "AIC|BIC|Log.Lik|Deviance|RMSE",  # omit extra stats
-  add_rows = NULL,
-  escape = FALSE  # to allow LaTeX symbols in labels
+  gof_omit = "AIC|BIC|Log.Lik|Deviance|RMSE",
+  escape = FALSE
 )
+
